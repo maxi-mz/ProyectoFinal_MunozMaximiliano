22dea624f887511fc5a3c60c57080dc1a83382a6
maxi-mz <mmunoz@dc.uba.ar>
2022-08-09 13:23:25 -0300

    Inicio banner

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
